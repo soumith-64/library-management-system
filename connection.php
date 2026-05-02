78443875c7610a56d0bf886957b046fbec900bd6
soumith-64 <141291462+soumith-64@users.noreply.github.com>
2026-05-02 08:55:42 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php 
-$link = mysqli_connect("localhost", "u663364821_root", "Wwis@2025");
+$link = mysqli_connect("localhost", "root", "pass");
 
 if (!$link) {
     die('Could not connect: ' . mysqli_connect_error());
 }
 
-mysqli_select_db($link, "u663364821_project");
+mysqli_select_db($link, "root");
 ?>
